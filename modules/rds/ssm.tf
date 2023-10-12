2210a60ef823ed4f48884aae995965b9c9c53cad
AgusPk <agustinperkins@hotmail.com>
2023-10-12 12:40:39 -0300

    changed parameter name

--- modules/rds/ssm.tf
+++ modules/rds/ssm.tf
@@ -1,23 +1,23 @@
-resource "aws_ssm_parameter" "db_address" {
-  name  = "/${var.name}/db_address"
+resource "aws_ssm_parameter" "address" {
+  name  = "/${var.name}/address"
   type  = "String"
   value = module.db.db_instance_address
 }
 
-resource "aws_ssm_parameter" "db_name" {
-  name  = "/${var.name}/db_name"
+resource "aws_ssm_parameter" "name" {
+  name  = "/${var.name}/name"
   type  = "String"
   value = module.db.db_instance_name
 }
 
-resource "aws_ssm_parameter" "db_port" {
-  name  = "/${var.name}/db_port"
+resource "aws_ssm_parameter" "port" {
+  name  = "/${var.name}/port"
   type  = "String"
   value = module.db.db_instance_port
 }
 
-resource "aws_ssm_parameter" "db_master_user_secret_arn" {
-  name  = "/${var.name}/db_master_user_secret_arn"
+resource "aws_ssm_parameter" "master_user_secret_arn" {
+  name  = "/${var.name}/master_user_secret_arn"
   type  = "String"
   value = module.db.db_instance_master_user_secret_arn
 }
