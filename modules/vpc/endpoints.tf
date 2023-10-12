ee8855512bdb33206db48019b57bf26849dea0b0
AgusPk <agustinperkins@hotmail.com>
2023-10-12 13:24:52 -0300

    fix lint

--- modules/vpc/endpoints.tf
+++ modules/vpc/endpoints.tf
@@ -5,6 +5,6 @@ resource "aws_vpc_endpoint" "secrets_manager" {
 
   service_name = "com.amazonaws.${data.aws_region.current.name}.secretsmanager"
 
-  security_group_ids = [module.app_security_group.security_group_id]  # You can specify security groups if needed
-  subnet_ids         = module.vpc.private_subnets  # Associate with private subnets
+  security_group_ids = [module.app_security_group.security_group_id] # You can specify security groups if needed
+  subnet_ids         = module.vpc.private_subnets                    # Associate with private subnets
 }
