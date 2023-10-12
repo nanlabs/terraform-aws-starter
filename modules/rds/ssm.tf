5d25fe9f348e328010ede029ceb2dd120e897a95
AgusPk <agustinperkins@hotmail.com>
2023-10-12 12:28:11 -0300

    changed parameter type

--- modules/rds/ssm.tf
+++ modules/rds/ssm.tf
@@ -1,23 +1,23 @@
-resource "aws_ssm_parameter" "db_instance_address" {
-  name  = "/${var.name}/db_instance_address"
-  type  = "StringList"
+resource "aws_ssm_parameter" "db_address" {
+  name  = "/${var.name}/db_address"
+  type  = "String"
   value = module.db.db_instance_address
 }
 
-resource "aws_ssm_parameter" "db_instance_name" {
-  name  = "/${var.name}/db_instance_name"
-  type  = "StringList"
+resource "aws_ssm_parameter" "db_name" {
+  name  = "/${var.name}/db_name"
+  type  = "String"
   value = module.db.db_instance_name
 }
 
-resource "aws_ssm_parameter" "db_instance_port" {
-  name  = "/${var.name}/db_instance_port"
-  type  = "StringList"
+resource "aws_ssm_parameter" "db_port" {
+  name  = "/${var.name}/db_port"
+  type  = "String"
   value = module.db.db_instance_port
 }
 
-resource "aws_ssm_parameter" "db_instance_master_user_secret_arn" {
-  name  = "/${var.name}/db_instance_master_user_secret_arn"
-  type  = "StringList"
+resource "aws_ssm_parameter" "db_master_user_secret_arn" {
+  name  = "/${var.name}/db_master_user_secret_arn"
+  type  = "String"
   value = module.db.db_instance_master_user_secret_arn
 }
