0302e04d451710a51083ebe5e395d960ed34ea0f
ulises-jeremias <ulisescf.24@gmail.com>
2024-07-07 23:13:25 -0300

    chore: Update required Terraform versions for modules

--- modules/mongodb/versions.tf
+++ modules/mongodb/versions.tf
@@ -1,10 +1,10 @@
 terraform {
+  required_version = ">= 0.12"
+
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
       version = "1.12.1"
     }
   }
-
-  required_version = ">= 0.12"
 }
