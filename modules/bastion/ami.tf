53871eecfe8201982bd85baa14ac44a2068377bf
ulises-jeremias <ulisescf.24@gmail.com>
2024-07-11 16:55:38 -0300

    chore: Update AWS AMI filter in bastion module

--- modules/bastion/ami.tf
+++ modules/bastion/ami.tf
@@ -3,6 +3,13 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
